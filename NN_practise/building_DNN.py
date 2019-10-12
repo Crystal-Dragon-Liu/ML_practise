c4f05b283c4ab8713cdbe5e7a99683a4db44cd0e
Crystal-Dragon-Liu <LybCrystal@163.com>
2019-10-12 20:59:12 +0800

    更新参数前

--- NN_practise/building_DNN.py
+++ NN_practise/building_DNN.py
@@ -229,7 +229,7 @@ def compute_cost(AL,Y):
     assert(cost.shape ==())
     return cost
 
-#反向传播
+#反向传播线性部分，即已知dZ直接求dW,db,dA_prev
 
 def linear_backward(dZ,cache):
     '''
@@ -247,6 +247,83 @@ def linear_backward(dZ,cache):
 
     '''
 
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+    dW = np.dot(dZ, A_prev.T) / m
+
+    db = np.sum(dZ,axis=1,keepdims=True) /m
+    dA_prev = np.dot(W.T, dZ)
+
+    assert(dA_prev.shape == A_prev.shape)
+    assert(dW.shape == W.shape)
+    assert(db.shape == b.shape)
+
+    return dA_prev, dW, db
+
+#线性激活部分反向传播 定义 sigmoid_backward用于实现sigmoid函数的反向传播 relu_backward实现relu函数的反向传播
+
+def linear_activation_backward(dA, cache, activation):
+    '''
+
+     实现LINEAR-> ACTIVATION层的后向传播。
+
+    参数：
+         dA - 当前层l的激活后的梯度值
+         cache - 我们存储的用于有效计算反向传播的值的元组（值为linear_cache，activation_cache）
+         activation - 要在此层中使用的激活函数名，字符串类型，【"sigmoid" | "relu"】
+    返回：
+         dA_prev - 相对于激活（前一层l-1）的成本梯度值，与A_prev维度相同
+         dW - 相对于W（当前层l）的成本梯度值，与W的维度相同
+         db - 相对于b（当前层l）的成本梯度值，与b的维度相同
+    '''
+    linear_cache, activation_cache = cache
+    if activation == "relu":
+        dZ = dnn_utils.sigmoid_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        return dA_prev, dW, db
+    if activation == "sigmoid":
+        dZ = dnn_utils.relu_backward(dA, activation_cache)
+        dA_prev, dW, db = linear_backward(dZ, linear_cache)
+        return dA_prev, dW, db
+
+#求多层模型的backward每层的梯度
+
+def L_model_backward(AL, Y, caches):
+
+    '''
+
+     对[LINEAR-> RELU] *（L-1） - > LINEAR - > SIGMOID组执行反向传播，就是多层网络的向后传播
+
+    参数：
+     AL - 概率向量，正向传播的输出（L_model_forward（））
+     Y - 标签向量（例如：如果不是猫，则为0，如果是猫则为1），维度为（1，数量）
+     caches - 包含以下内容的cache列表：
+                 linear_activation_forward（"relu"）的cache，不包含输出层
+                 linear_activation_forward（"sigmoid"）的cache
+
+    返回：
+     grads - 具有梯度值的字典
+              grads [“dA”+ str（l）] = ...
+              grads [“dW”+ str（l）] = ...
+              grads [“db”+ str（l）] = ...
+
+    '''
+    grads = {}
+    L = len(caches) #层数
+    m = AL.shape[1] #样本数
+    Y = Y.reshape(AL.shape)
+    dAL = - (np.divide(Y, AL) - np.divide(1-Y, 1-AL))
+
+    current_cache = caches[L-1]
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache,"sigmoid")
+    for l in reversed(range(L-1)):
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(l+2)], current_cache, "relu")
+
+        grads["dA" + str(l+1)] = dA_prev_temp
+        grads["dW" + str(l+1)] = dA_prev_temp
+        grads["db" + str(l+1)] = db_temp
+    return grads
 
 
 #print(Y.shape)
@@ -298,6 +375,43 @@ print("=====compute_cost=====")
 Y,AL = testCase.compute_cost_test_case()
 cost = compute_cost(AL, Y)
 print("cost = "+str(cost))
+
+
+#反向传播线性部分
+print("=====linear_backforward=====")
+dZ, linear_cache = testCase.linear_backward_test_case()
+dA_prev, dW, db = linear_backward(dZ, linear_cache)
+
+print("dA_prev = " + str(dA_prev)+"  the shape of dA_prev is :"+ str(dA_prev.shape))
+print("dW = " + str(dW)+"  the shape of dW is : "+ str(dW.shape))
+print("db = " + str(db)+"  the shape of db is : "+ str(db.shape))
+
+
+#反向传播线性激活部分
+print("=====linear_activation_backward=====")
+AL, linear_activation_cache = testCase.linear_activation_backward_test_case()
+# (testCase.linear_activation_backward_test_case())    return dA, linear_activation_cache
+#linear_cache = (A, W, b)
+#activation_cache = Z
+#linear_activation_cache = (linear_cache, activation_cache)
+dA_prev, dW, db = linear_activation_backward(AL, linear_activation_cache, activation="sigmoid")
+print("relu")
+print("dA_prev = " + str(dA_prev))
+print("dW = " + str(dW))
+print("db = " + str(db) + "\n")
+
+
+
+#求多层模型的backward每层的梯度
+print("=====L_model_backward=====")
+AL, Y_assess, caches = testCase.L_model_backward_test_case()
+grads = L_model_backward(AL, Y_assess, caches)
+
+print("dW1 = " + str(grads["dW1"]))
+print("db1 = " + str(grads["db1"]))
+print("dA1 = " + str(grads["dA1"]))
+print(grads)
+
 
 '''
 
